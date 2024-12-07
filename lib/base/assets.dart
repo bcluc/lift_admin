c4c5a165419554f126a1fdee0c16352abfb43d2f
bcluc <cuongb.12903@gmail.com>
2024-12-08 06:24:52 +0700

    login screen

---
+++ lib/base/assets.dart
@@ -0,0 +1,6 @@
+class Assets {
+  // IMAGES
+  static const String _images = 'assets/images/';
+  static const String _logos = 'assets/logos/';
+  static const String logo = '${_logos}logo.jpg';
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:lift_admin/base/assets.dart';
+import 'package:shimmer/shimmer.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  Future<void> _redirect() async {
+    await Future.delayed(const Duration(seconds: 2));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Executes a function only one time after the layout is completed
+    WidgetsBinding.instance.addPostFrameCallback((_) => _redirect());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      alignment: Alignment.center,
+      child: Shimmer.fromColors(
+        baseColor: Theme.of(context).colorScheme.primary,
+        highlightColor: Colors.amber,
+        child: Image.asset(
+          Assets.logo,
+          width: 300,
+        ),
+      ),
+    );
+  }
+}
